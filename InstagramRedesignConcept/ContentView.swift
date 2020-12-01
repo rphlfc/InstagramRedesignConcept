314819d57e3a4f2865f713d5036863e91a9fa959
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-12-01 20:07:13 -0300

    Initial Commit

---
+++ InstagramRedesignConcept/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramRedesignConcept
+//
+//  Created by Raphael Cerqueira on 01/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramRedesignConcept/InstagramRedesignConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramRedesignConceptApp.swift
+//  InstagramRedesignConcept
+//
+//  Created by Raphael Cerqueira on 01/12/20.
+//
+
+import SwiftUI
+
+@main
+struct InstagramRedesignConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
